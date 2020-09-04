3a69f0146ea99002ac5414919e64e1635ee12cae
bobwmcgrath <bobwmcgrath@gmail.com>
2020-09-04 06:37:44 -0500

    working example

--- app/src/main/java/com/example/mqttreberrypie/Constants.java
+++ app/src/main/java/com/example/mqttreberrypie/Constants.java
@@ -2,9 +2,9 @@ package com.example.mqttreberrypie;
 
 public class Constants {
 
-    public static final String MQTT_BROKER_URL = "tcp://192.168.1.6:1883";
+    public static final String MQTT_BROKER_URL = "tcp://192.168.1.2:1883";
 
-    public static final String PUBLISH_TOPIC = "androidkt/topic";
+    public static final String PUBLISH_TOPIC = "test";
 
     public static final String CLIENT_ID = "androidkt";
 }
--- app/src/main/java/com/example/mqttreberrypie/MainActivity.java
+++ app/src/main/java/com/example/mqttreberrypie/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.mqttreberrypie;
 
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,8 +14,6 @@ import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,99 +22,66 @@ public class MainActivity extends AppCompatActivity {
     private PahoMqttClient pahoMqttClient;
 
     private EditText textMessage, subscribeTopic, unSubscribeTopic;
-    private Button XMYP,Z;
-    private ListView mVideosListView;
-    private List<Video> mVideosList = new ArrayList<>();
-    private VideoAdapter mVideoAdapter;
+    private Button publishMessage, subscribe, unSubscribe;
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pahoMqttClient = new PahoMqttClient();
-        XMYP=findViewById(R.id.XmYp);
-       Z=findViewById(R.id.z);
-        mVideosListView = (ListView) findViewById(R.id.videoListView);
 
-        //create videos
-        Video projectvideo = new Video("https://s3.amazonaws.com/androidvideostutorial/862009639.mp4");
-        mVideosList.add(projectvideo);
-        mVideoAdapter = new VideoAdapter(this, mVideosList);
-        mVideosListView.setAdapter(mVideoAdapter);
+        textMessage = (EditText) findViewById(R.id.textMessage);
+        publishMessage = (Button) findViewById(R.id.publishMessage);
 
+        subscribe = (Button) findViewById(R.id.subscribe);
+        unSubscribe = (Button) findViewById(R.id.unSubscribe);
 
-
-
-
-
+        subscribeTopic = (EditText) findViewById(R.id.subscribeTopic);
+        unSubscribeTopic = (EditText) findViewById(R.id.unSubscribeTopic);
         client = pahoMqttClient.getMqttClient(getApplicationContext(), Constants.MQTT_BROKER_URL, Constants.CLIENT_ID);
 
+        publishMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = textMessage.getText().toString().trim();
+                if (!msg.isEmpty()) {
+                    try {
+                        pahoMqttClient.publishMessage(client, msg, 1, Constants.PUBLISH_TOPIC);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
-     XMYP.setOnTouchListener(new View.OnTouchListener() {
-         @Override
-         public boolean onTouch(View view, MotionEvent motionEvent) {
-             if(motionEvent.getAction() == MotionEvent.ACTION_BUTTON_PRESS){
-                 try {
-                     pahoMqttClient.publishMessage(client, "payload='go(xaxis,-1)'", 1, "motor");
-                     pahoMqttClient.publishMessage(client, "payload='go(yaxis,1)'", 1,"motor");
-                 } catch (MqttException e) {
-                     e.printStackTrace();
-                 } catch (UnsupportedEncodingException e) {
-                     e.printStackTrace();
-                 }
-                 // Do what you want
-                 return true;
-             }
-          else   if(motionEvent.getAction()==MotionEvent.ACTION_BUTTON_RELEASE)
-             {
-                 try {
-                     pahoMqttClient.publishMessage(client, "payload='stop(xaxis)'", 1, "motor");
-                     pahoMqttClient.publishMessage(client, "payload='stop(yaxis)'", 1, "motor");
-                 } catch (MqttException e) {
-                     e.printStackTrace();
-                 } catch (UnsupportedEncodingException e) {
-                     e.printStackTrace();
-                 }
-                 return true;
-             }
-
-
-             return false;
-         }
-     });
-     Z.setOnTouchListener(new View.OnTouchListener() {
-         @Override
-         public boolean onTouch(View view, MotionEvent motionEvent) {
-             if(motionEvent.getAction() == MotionEvent.ACTION_BUTTON_PRESS){
-                 try {
-                     pahoMqttClient.publishMessage(client, "payload='go(zaxis,1)'", 1, "motor");
-
-                 } catch (MqttException e) {
-                     e.printStackTrace();
-                 } catch (UnsupportedEncodingException e) {
-                     e.printStackTrace();
-                 }
-                 // Do what you want
-                 return true;
-             }
-          else   if(motionEvent.getAction()==MotionEvent.ACTION_BUTTON_RELEASE)
-             {
-                 try {
-                     pahoMqttClient.publishMessage(client, "payload='stop(zaxis)'", 1, "motor");
-
-                 } catch (MqttException e) {
-                     e.printStackTrace();
-                 } catch (UnsupportedEncodingException e) {
-                     e.printStackTrace();
-                 }
-                 return true;
-             }
-             return false;
-         }
-     });
-
-
+        subscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String topic = subscribeTopic.getText().toString().trim();
+                if (!topic.isEmpty()) {
+                    try {
+                        pahoMqttClient.subscribe(client, topic, 1);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        unSubscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String topic = unSubscribeTopic.getText().toString().trim();
+                if (!topic.isEmpty()) {
+                    try {
+                        pahoMqttClient.unSubscribe(client, topic);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
         Intent intent = new Intent(MainActivity.this, MqttMessageService.class);
         startService(intent);
